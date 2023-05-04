34cce50ed70165ad54a232a794e09539f72fbfa6
mihaidouss <m_haidouss@hotmail.com>
2023-05-04 19:11:06 +0200

    Scrape data by click

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function scrapeData() {
     fetch('/scrape', {
         method: 'POST'
     })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error));
 }
